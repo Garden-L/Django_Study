58bba33c9f09d597b82ab198bd1038b9b92e26ab
JeongWonLim <cdnljw@gmail.com>
2022-04-07 17:19:37 +0900

    로그인 로그아웃

--- pybo/views.py
+++ pybo/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question, Answer
-from .forms import QuestionForm
+from .forms import QuestionForm, AnswerForm
+from django.http import HttpResponseNotAllowed
+from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
+    page = request.GET.get('page', '1') #페이지
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list' : question_list}
-    print(question_list)
+    paginator = Paginator(question_list,10)
+    page_obj = paginator.get_page(page)
+    context={'question_list' : page_obj}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
@@ -22,5 +26,31 @@ def answer_create(request, question_id):
     return redirect('pybo:detail', question_id=question.id)
 
 def question_create(request):
-    form = QuestionForm()
-    return render(request, 'pybo/question_form.html', {'form':form})
+    if request.method =='POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pyho:index')
+    else:
+        form =QuestionForm()
+    context = {'form' : form}
+    return render(request, 'pybo/question_form.html', context)
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id = question.id)
+    else:
+        return HttpResponseNotAllowed('Only Post is possible')
+    context = {'question' : question, 'form': form}
+    
+    return render(request, 'pybo/question_detail.html', context)
